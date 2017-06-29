1a372a8451a8e78bcdeeb363bb93991afd56623c
Abdullah Clute <clute.abdullahb@gmail.com>
2017-06-29 11:41:19 -0500

    Added details page

--- theviews/urls.py
+++ theviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
+    url(r'^([0-9]+)/$', views.details, name='details'),
 ]
--- theviews/views.py
+++ theviews/views.py
@@ -10,3 +10,7 @@ def product_list(request):
     cats = Cat.objects.all()
     brands = Brand.objects.all()
     return render(request, 'index.html', {'products': products, 'brands': brands, 'cats': cats})
+
+def details(request, pro_id):
+    product = Product.objects.get(id=pro_id)
+    return render(request, 'details.html', {'product': product})
